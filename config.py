c0d8d28d1540a7a0849db92e87cb15d38de01cce
Tatuck <59171409+Tatuck@users.noreply.github.com>
2021-08-30 03:54:03 +0200

    Edit config.py
    Remove my credentials as I leaked them :D

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-GITHUB_USERNAME = "Tatuck"
+GITHUB_USERNAME = "<github-username>"
 GITHUB_URL = "https://api.github.com"
-DISCORD_URL = "https://discordapp.com/api/webhooks/881332584695599104/5PJGo9tRdIgt5Gx9H9Ko0QMoXGClWDmLraYZ2RwhgIllKFDcbamF4WK3ip0Y6lJEFiam"
+DISCORD_URL = "<discord-webhook>"
 TIME_PER_REQUEST = 60 #X-RateLimit-Limit on the headers of the request
